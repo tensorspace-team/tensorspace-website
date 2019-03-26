d9b012d544b42f50abd82fdca1964955982a541b
syt123450 <421537891@qq.com>
2019-03-26 14:13:05 -0700

    Docs: Add new tag

---
+++ js/converterDoc.js
@@ -0,0 +1,9 @@
+$(function() {
+	$("#close").hover(function() {
+		$("#close").attr("src", "../assets/img/docs/close_hover.png");
+	}, function() {
+		$("#close").attr("src", "../assets/img/docs/close.png");
+	}).click(function() {
+		moveOutHiddenContent();
+	});
+});
